592adf53b8221ee85fe3237a712f873f63c8a8f0
Ryan Boudwin <ryanboudwin@outlook.com>
2025-01-25 11:08:45 +0200

    portal placeholder moves left and right

---
+++ portal.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var SPEED = 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float):
+	position.x += SPEED
+	if position.x >= 950:
+		SPEED -=2
+	if position.x <= 0:
+		SPEED +=2
