17f03ba98d0066a7762874d48c07ca475b298ec2
Ryan Boudwin <ryanboudwin@outlook.com>
2025-01-25 15:05:24 +0200

    now spawning enemies from portal position

--- scenes/area_2d.gd
+++ scenes/area_2d.gd
@@ -2,11 +2,17 @@ extends Area2D
 
 var SPEED = 2
 
+@export var mob_scene: PackedScene
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$MobTimer.timeout.connect(_on_timer_timeout)
 
-
+func _on_timer_timeout():
+	if mob_scene:
+		var mob = mob_scene.instantiate()
+		get_parent().add_child(mob)  # Add mob to the scene
+		mob.global_position.x = global_position.x + 25 # Spawn at Area2D's position
+		mob.global_position.y = global_position.y - 150
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
