063314f083a3ba5247df80ad76acfd8d623a9de0
kootee <katjatoivola@icloud.com>
2025-01-26 12:43:11 +0200

    adds title screen

---
+++ scenes/title_screen.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("enter_key"):
+		get_tree().change_scene_to_file("res://scenes/level_1.tscn")
