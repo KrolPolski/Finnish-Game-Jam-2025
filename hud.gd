82129faf663f50da58cfbe557006d61a0851ede4
kootee <katjatoivola@icloud.com>
2025-01-26 14:22:08 +0200

    adds timer to HUD

--- hud.gd
+++ hud.gd
@@ -1,15 +1,26 @@
 extends CanvasLayer
 
+var time = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	time += delta
 	update_score(get_parent().get_node("Portal2").score)
+	update_health(get_parent().get_node("Player").health)
+	update_timer(time)
 
 func update_score(score):
 	$ScoreLable/ScoreCounter.text = str(score)
+
+func update_health(health):
+	$Health.text = str(health)
+
+func update_timer(time):
+	var formatted_time = "%0.1f" % time
+	$TimerLable/TimerCounter.text = str(formatted_time)
 
 func show_message(text):
 	$Message.text = text
